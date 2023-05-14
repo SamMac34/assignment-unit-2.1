0c3b1ebc385e0d0ce314d117cfdb17bb677510a8
Sam McCauley <sammccauley34@gmail.com>
2023-05-14 12:13:25 -0500

    Changed value for variable fullName from defined string to existing var+var

--- assignment/scripts/aboutMe.js
+++ assignment/scripts/aboutMe.js
@@ -7,7 +7,7 @@ let lastName = 'McCauley';
 console.log( 'Last Name:', lastName );
 // 3 - Create a third variable called `fullName`, assign it the value of your first and last name
 // (remember, you already have variables for this, can you use those?)
-let fullName = 'Sam McCauley';
+let fullName = firstName + lastName;
 // 4 - Console log the value of `fullName`
 console.log( 'Full name:', fullName );
 // 5 - Create a variable called `luckyNumber` and assign it the value of your lucky number.
